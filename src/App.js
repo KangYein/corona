c299fe5a3bfca7bf3a6c0d416a9744f64e657327
KangYein <yeinkang04@gmail.com>
2021-12-26 17:04:44 +0900

    corona 통계 사이트: postman 사용

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import Contents from './components/Contents'
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Contents />
+      
+    </div>
+  );
+}
+
+export default App;
